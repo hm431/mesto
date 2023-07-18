cff113ce1e03b735c39b5405eb3debb830923479
Andrey Chuchalin <aic7120@gmail.com>
2023-07-18 10:30:46 +0300

    Исправлены ошибки

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,18 @@ let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__container');
 let editButton = document.querySelector('.profile__edit-button');
 let formClose = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup_input__name');
-let jobInput = document.querySelector('.popup_input__status');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_status');
 let startName = document.querySelector('.profile__name');
 let startStatus = document.querySelector('.profile__status');
 let popupSave = document.querySelector('.popup__save');
-document.querySelector('.popup_input__name').placeholder = startName.textContent;
-document.querySelector('.popup_input__status').placeholder = startStatus.textContent;
 
+// Переводим данные о профиле со страницы в попап
 
+document.querySelector('.popup__input_name').placeholder = startName.textContent;
+document.querySelector('.popup__input_status').placeholder = startStatus.textContent;
+
+//Функция, которая 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     document.querySelector('.profile__name').textContent = nameInput.value;
@@ -20,21 +23,20 @@ function handleFormSubmit (evt) {
 }
 
 
-
+//Функция добавления класса popup__open, который делает видимым popup
 function oupenForm (){ 
-    popup.style.display = "flex"; 
-    container.style.background = "rgba(0, 0, 0, 0.15)";
+    popup.classList.add("popup__open");
+    //popup.style.display = "flex"; 
+    //container.style.background = "rgba(0, 0, 0, 0.15)";
 }
 
 function closeForm() {
-    popup.style.display = "none"; 
+    popup.classList.remove("popup__open"); 
 }
 
 editButton.addEventListener('click', oupenForm);
 formClose.addEventListener('click', closeForm);
 popupSave.addEventListener('click', handleFormSubmit);
-
-
 formElement.addEventListener('submit', handleFormSubmit);
 
 
