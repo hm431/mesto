3f08f56cc581e519cf56273b851d6b7564c1eeb4
Andrey Chuchalin <aic7120@gmail.com>
2023-07-24 17:59:03 +0300

    исправлены ошибки

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,39 @@
-let container = document.querySelector('.main');
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container');
+let WindowElement = document.querySelector('.popup__container');
 let editButton = document.querySelector('.profile__edit-button');
 let formClose = document.querySelector('.popup__close');
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_status');
 let startName = document.querySelector('.profile__name');
 let startStatus = document.querySelector('.profile__status');
-let popupSave = document.querySelector('.popup__save');
 
 // Переводим данные о профиле со страницы в попап
 
-document.querySelector('.popup__input_name').placeholder = startName.textContent;
-document.querySelector('.popup__input_status').placeholder = startStatus.textContent;
+document.querySelector('.popup__input_name').value = startName.textContent;
+document.querySelector('.popup__input_status').value = startStatus.textContent;
 
 //Функция, которая 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-    document.querySelector('.profile__name').textContent = nameInput.value;
-    document.querySelector('.profile__status').textContent = jobInput.value;   
-  
+    startName.textContent = nameInput.value;
+    startStatus.textContent = jobInput.value;   
 }
 
 
 //Функция добавления класса popup__open, который делает видимым popup
 function oupenForm (){ 
-    popup.classList.add("popup__open");
-    //popup.style.display = "flex"; 
-    //container.style.background = "rgba(0, 0, 0, 0.15)";
+    nameInput.value = startName.textContent;
+    jobInput.value = startStatus.textContent;   
+    popup.classList.add("popup_opened");
 }
 
 function closeForm() {
-    popup.classList.remove("popup__open"); 
+    popup.classList.remove("popup_opened"); 
 }
 
 editButton.addEventListener('click', oupenForm);
 formClose.addEventListener('click', closeForm);
-popupSave.addEventListener('click', handleFormSubmit);
-formElement.addEventListener('submit', handleFormSubmit);
+WindowElement.addEventListener('submit', handleFormSubmit);
 
 
 
